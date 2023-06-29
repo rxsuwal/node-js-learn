ac738f03aad5d0c0101b7aba2532244160a67b57
ranjan suwal <work.rsuwal@gmail.com>
2023-06-29 22:57:22 +0545

    web server styling templating with hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,23 +1,48 @@
 const express = require('express')
 const path = require('path')
+const hbs = require('hbs')
+
+const app = express()
+
+
+const viewPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // console.log(__dirname)
 // console.log(path.join(__dirname, '../public'))
 
-const app = express()
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+hbs.registerPartials(partialsPath)
 
+// SERVE STATIC HTML
 const publicPathDirectory = path.join(__dirname, '../public')
 
 app.use(express.static(publicPathDirectory))
 
-// const aboutPath = path.join(__dirname, '../public/about.html')
 
-// app.use('/about',express.static(aboutPath))
+// HANDLE BARS HTML TEMPLATE
 
-// const helpPath = path.join(__dirname, '../public/help.html')
 
-// app.use('/help',express.static(helpPath))
+app.get('', (req, res) => {
+    res.render('index', { title: 'TITLE FOR HBS' })
+})
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'about' })
+})
+app.get('/help', (req, res) => {
+    res.render('help', { title: 'help' })
+})
 
+
+app.get('/help/*', (req, res) => {
+    res.render('404', { msg: 'help data not found' })
+})
+app.get('*', (req, res) => {
+    res.render('404', { msg: 'page not found' })
+})
+
+// // BASIC ROUTE SERVICE 
 // app.get('', (req, res) => {
 //     res.send("hello express")
 // })
