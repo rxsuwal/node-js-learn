7e5ad4e7c4d98399de4fce6b53bd726751e3be2b
ranjan suwal <work.rsuwal@gmail.com>
2023-07-19 11:21:40 +0545

    weather server complete

---
+++ resumedata.js
@@ -0,0 +1,71 @@
+const experience = [
+    {
+        company: {
+            name: "Fourth Valley Concierge Corp.",
+            address: "Tokyo, Japan",
+            work_model: "remote"
+        },
+        position: "Frontend Engineer",
+        working_period: {
+            start: "nov-2022",
+            end: "jul-2023"
+        },
+        responsibilites: [
+            "Continous improvement of UI/UX using component libraries like react-bootstrap for with scalability in ReactJS",
+            "End-to-end backend application integration using GraphQL API's",
+            "Project tracking with agile methodology (scrum) on JIRA",
+            "Maintain CI/CD pipelines through git version control system and AWS assisting DevOps engineer",
+            "Perform unit tests with jest with automated testing & monitoring ",
+        ],
+        projects: [
+            "https://job.connectiu.com",
+            "https://studyabroad.connectiu.com"
+        ]
+    },
+
+    {
+        company: {
+            name: "Cellcom Pvt. Ltd.(CellPay)",
+            address: "Kathmandu,Nepal",
+            location_type: "onsite"
+        },
+        position: "Frontend Developer",
+        working_period: {
+            start: "feb-2020",
+            end: "nov-2022"
+        },
+        responsibilites: [
+            "Developed bank link feature on web app implementing REST API on react.js",
+            "Lead CRM Development in cross-functional collaboration with backend(JAVA) developers",
+            "Optimise 10% web performance using redux for state management",
+            "Develop strategy/work flow for checkout/web SDK for payment integration",
+        ],
+        projects: [
+            "https://cellpay.com.np",
+            "CellPay CRM"
+        ]
+    },
+
+    {
+        company: {
+            name: "Incwell Venture",
+            address: "Lalitpur,Nepal",
+            location_type: "onsite"
+        },
+        position: "Intern",
+        working_period: {
+            start: "nov-2019",
+            end: "feb-2020"
+        },
+        responsibilites: [
+            "Continuous learning web development with HTML, css, javascript and CMS wordpress",
+            "Study web standards & lifecycle with self motivation & other problem solving skills using Javascript",
+            "Undestand web design principle with SEO optimization",
+            "Communicate with team for collaborative work & solutions",
+            "UX / UI design using figma",
+        ]
+    },
+
+
+]
+
